cd9274fd99f1b17ed77f851e301f44d2232f0608
Chris <chris@justanswer.com>
2017-04-24 22:52:00 -0700

    db

--- craft/config/db.php
+++ craft/config/db.php
@@ -10,16 +10,16 @@
 return array(
 
 	// The database server name or IP address. Usually this is 'localhost' or '127.0.0.1'.
-	'server' => 'localhost',
+	'server' => 'wvulqmhjj9tbtc1w.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
 
 	// The name of the database to select.
-	'database' => '',
+	'database' => 'jub2vjrtfsp9b39t',
 
 	// The database username to connect with.
-	'user' => 'root',
+	'user' => 'a4iww03s2x8blwjj',
 
 	// The database password to connect with.
-	'password' => '',
+	'password' => 'v137a0njodedn1te',
 
 	// The prefix to use when naming tables. This can be no more than 5 characters.
 	'tablePrefix' => 'craft',
